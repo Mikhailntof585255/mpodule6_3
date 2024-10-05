041f72a6c244df330936816737a02689523f2ab8
module3_last2 <unisport2006@mail.ru>
2024-10-05 12:10:10 +0300

    Update module6_3.py

--- module6_3.py
+++ module6_3.py
@@ -1,39 +1,51 @@
 class Horse:
     x_distance = 0
     sound = 'Frrr'
-    def run(self,dx):
-        self.dx=dx
-        self.x_distance=self.x_distance+dx
+
+    def __init__(self,x_distance):
+        self.x_distance=x_distance
+
+    def run(self, dx):
+        self.dx = dx
+        self.x_distance = self.x_distance + dx
         return self.x_distance
+
 
 class Eagl:
     y_distance = 0
     sound = 'I train, eat, sleep, and repeat'
-    def fly(self,dy):
-        self.dy=dy
-        self.y_distance=self.y_distance+dy
+
+    def __init__(self,y_distance):
+        self.y_distance=y_distance
+
+    def fly(self, dy):
+        self.dy = dy
+        self.y_distance = self.y_distance + dy
         return self.y_distance
 
-class Pegasus(Horse,Eagl):
-    def move(self,dx,dy):
+
+class Pegasus(Horse, Eagl):
+    def __init__(self,x_distance,y_distance):
+        self.x_distance=x_distance
+        self.y_distance=y_distance
+
+    def move(self, dx, dy):
         super().run(dx)
         super().fly(dy)
-        return self.x_distance,self.y_distance
+        return self.x_distance, self.y_distance
+
     def get_pas(self):
-        return (self.x_distance,self.y_distance)
-    def voice (self):
+        return (self.x_distance, self.y_distance)
+
+    def voice(self):
         print(Eagl.sound)
 
 
 
 
 
-p1=Pegasus()
-print(p1.get_pas())
-p1.move(10,15)
-print(p1.get_pas())
-p1.move(-5,20)
-print(p1.get_pas())
-p1.voice()
+
+
+
 
 
